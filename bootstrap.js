65ddc7d0314aaf8d3a833c91e944220aa94a8312
Theo Browne <theo@theobrowne.com>
2015-02-25 03:41:32 -0500

    Cleaning/Commenting

--- bootstrap.js
+++ bootstrap.js
@@ -2,27 +2,8 @@ function addToQueue(info, queueContent){
 	//To be written
 }
 
-// function genericOnClick(info){ //This is the on-click code, everything important should be here
-// 	//console.log("item " + info.menuItemId + " was clicked");
-// 	//console.log("info: " + JSON.stringify(info));
-// 	//console.log("tab: " + JSON.stringify(tab));
 
-// 	//Uncomment to have click open new tab
-// 	//chrome.tabs.create({ url: info.linkUrl, active: false});
-// 	var d = new Date();
-// 	var queueContent = { url: info.linkURL, timeAdded: d.getTime() };
-
-// 	console.log("New URL object was created with URL: " + queueContent.url + " at time " + queueContent.timeAdded);
-
-// 	chrome.tabs.create({ url: queueContent.url, active: false});
-
-
-// 	//alert("Sorry, we're still working on this feature. We'll fix it soon, we promise!");
-
-// }
-
-
-//This is the code that adds the context items
+//Adds context items
 var contexts = ["link"];
 for (var i = 0; i < contexts.length; i++){
 	var context = contexts[i];
@@ -32,9 +13,10 @@ for (var i = 0; i < contexts.length; i++){
 	console.log("'" + context + "' item:" + id);
 }
 
-
+//Listens for contextMenu button clicks
 chrome.contextMenus.onClicked.addListener(function(info, tab){
 
+	//Makes queueContent object with the clicked URL + the time it was added
 	var d = new Date();
 	var queueContent = { url: info.linkUrl, timeAdded: d.getTime() };
 
