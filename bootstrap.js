3fa89b90b429f51c18b7fb1e1c3597fafa31cb40
amanz360 <amanz360@gmail.com>
2015-02-25 22:04:30 -0500

    added parseID function
    parseID takes in a youtube link and returns the video ID. I also had it print out to the console the video id when a video is queued.

--- bootstrap.js
+++ bootstrap.js
@@ -1,8 +1,19 @@
 function printQueue(queue){
-	console.log("Printint Queue \n")
+
 	for(var i = 0; i < queue.length; i++){
 		console.log(queue[i].url + "\n");
 	}
+}
+
+//Helper function to parse the video id out of a youtube link
+function parseID(link)
+{
+	//Split the youtube link on the watch text
+	var results = link.split("/watch?v=");
+	//Grab the video id and store it in 'id'
+	var id = results[1];
+	//Returns the video id
+	return id;
 }
 
 
@@ -16,15 +27,7 @@ for (var i = 0; i < contexts.length; i++){
 	console.log("'" + context + "' item:" + id);
 }
 
-var queue;
-
-chrome.storage.sync.get('queue', function (result) {
-	if(result.length > 0){
-		queue = result;
-	}else{
-		queue = [];
-	}
-});
+var queue = [];
 
 //Listens for contextMenu button clicks
 chrome.contextMenus.onClicked.addListener(function(info, tab){
@@ -33,18 +36,23 @@ chrome.contextMenus.onClicked.addListener(function(info, tab){
 	var d = new Date();
 	var queueContent = { url: info.linkUrl, timeAdded: d.getTime() };
 
+	//Get the link from the queueContent object and pass it into the parseID method.
+	//parseID returns the video id in the youtube link and stores it in 'videoID'
+	//videoID becomes a property of queueContent underneath queueContent.videoID
+	var videoID = parseID(queueContent.url);
+	queueContent.videoID = videoID;
+
 	console.log("New URL object was created with URL: " + queueContent.url + " at time " + queueContent.timeAdded);
+	console.log("Video domain for URL object is: " + queueContent.videoID);
 
 	//Uncomment to create tab with queue'd URL
 	//chrome.tabs.create({ url: queueContent.url, active: false});
 
 	queue.push(queueContent);
 
+	console.log("Printint Queue \n")
 	printQueue(queue);
-	chrome.storage.sync.set({'queue': queue}, function() { console.log('Queue saved'); });
 
-	//alert("Sorry, we're still working on this feature. We'll fix it soon, we promise!");
+	alert("Sorry, we're still working on this feature. We'll fix it soon, we promise!");
 });
-
-
 
