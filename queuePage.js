b7e7506be21273830474e7e46a71521f2082b6a5
Theo Browne <theo@theobrowne.com>
2015-03-04 03:32:24 -0500

    Loads the first video in your queue. Also a total mess.

--- queuePage.js
+++ queuePage.js
@@ -1,76 +1,79 @@
-// @description Easier way to implement the YouTube JavaScript API
-// @author      Rob W
-// @global      getFrameID(id) Quick way to find the iframe object which corresponds to the given ID.
-// @global      YT_ready(Function:function [, Boolean:qeue_at_start])
-// @global      onYouTubePlayerAPIReady()  - Used to trigger the qeued functions
-// @website     http://stackoverflow.com/a/7988536/938089?listening-for-youtube-event-in-javascript-or-jquery
 
-function getFrameID(id){
-    var elem = document.getElementById(id);
-    if (elem) {
-        if(/^iframe$/i.test(elem.tagName)) return id; //Frame, OK
-        // else: Look for frame
-        var elems = elem.getElementsByTagName("iframe");
-        if (!elems.length) return null; //No iframe found, FAILURE
-        for (var i=0; i<elems.length; i++) {
-           if (/^https?:\/\/(?:www\.)?youtube(?:-nocookie)?\.com(\/|$)/i.test(elems[i].src)) break;
-        }
-        elem = elems[i]; //The only, or the best iFrame
-        if (elem.id) return elem.id; //Existing ID, return it
-        // else: Create a new ID
-        do { //Keep postfixing `-frame` until the ID is unique
-            id += "-frame";
-        } while (document.getElementById(id));
-        elem.id = id;
-        return id;
+
+var tag = document.createElement('script');
+tag.src = "//www.youtube.com/player_api";
+
+var loadQueueValue = function (callback){
+  chrome.storage.sync.get("queueObj", callback);
+};
+
+var queueObj = {queue: []};
+queueObj.loadQueueValue = loadQueueValue;
+//Load in the stored queue value.
+queueObj.loadQueueValue(function(result){
+  if(result["queueObj"] != undefined){
+    queueObj = result["queueObj"];
+    if(queueObj.queue == undefined){
+      queueObj.queue = [];
     }
-    // If no element, return null.
-    return null;
-}
+  }
+  setVideo();
+});
 
-// Define YT_ready function.
-var YT_ready = (function(){
-    var onReady_funcs = [], api_isReady = false;
-    /* @param func function     Function to execute on ready
-     * @param func Boolean      If true, all qeued functions are executed
-     * @param b_before Boolean  If true, the func will added to the first
-                                 position in the queue*/
-    return function(func, b_before){
-        if (func === true) {
-            api_isReady = true;
-            for (var i=0; i<onReady_funcs.length; i++){
-                // Removes the first func from the array, and execute func
-                onReady_funcs.shift()();
-            }
-        }
-        else if(typeof func == "function") {
-            if (api_isReady) func();
-            else onReady_funcs[b_before?"unshift":"push"](func); 
-        }
-    }
-})();
-// This function will be called when the API is fully loaded
-function onYouTubePlayerAPIReady() {YT_ready(true);}
+printQueue(queueObj.queue);
 
-// Load YouTube Frame API
-(function(){ //Closure, to not leak to the scope
-  var s = document.createElement("script");
-  s.src = "https://www.youtube.com/player_api"; /* Load Player API*/
-  var before = document.getElementsByTagName("script")[0];
-  before.parentNode.insertBefore(s, before);
-})();
 
 
 var player;
+console.log("queuePage.js loaded!");
+
+// var element = document.createElement("script");
+// element.src = "http://www.youtube.com/player_api";
+// document.body.appendChild(element);
+// console.log("Youtube API added");
+
+function iFrameOfID(ID){
+  var HTML = '<iframe width="640" height="390" frameborder="0" title="YouTube video player" type="text/html" src="http://www.youtube.com/embed/'
+  HTML = HTML + ID;
+  HTML = HTML + '?enablejsapi=1"></iframe>'
+  return HTML;
+}
+
+function setVideo(){
+
+  document.getElementById('player').innerHTML = iFrameOfID(queueObj.queue[0].videoID);
+  
+  callPlayer("player", function() {
+      // This function runs once the player is ready ("onYouTubePlayerReady")
+      callPlayer("player", "playVideo");
+  });
+}
+function printQueue(queue){
+
+  console.log("Printing Queue \n");
+  for(var i = 0; i < queue.length; i++){
+    console.log(queue[i].url + "\n");
+  }
+}
+
+/*function downloadJSAtOnload() {
+  var element = document.createElement("script");
+  element.src = "https://www.youtube.com/iframe_api";
+  document.body.appendChild(element);
+  console.log("Youtube API added");
+}
+
+function onClientLoad() {
+  console.log("client loaded!");
+    gapi.client.load('youtube', 'v3', onYouTubePlayerAPIReady);
+}
+
 function onYouTubePlayerAPIReady() {
   player = new YT.Player('player', {
-  videoId: 'rOXR5drPJlg',
-  events: {
-  'onReady': onPlayerReady,
-  'onStateChange': onPlayerStateChange
-}
-    });
-}
+    videoId: 'rOXR5drPJlg',
+    events: {'onReady': onPlayerReady, 'onStateChange': onPlayerStateChange }
+  });
+}*/
 //Plays video automatically
 function onPlayerReady(event) {
     event.target.playVideo();
@@ -82,3 +85,95 @@ function onPlayerStateChange(event) {
     //load new video ID
   }
 }
+
+
+/**
+ * @author       Rob W <gwnRob@gmail.com>
+ * @website      http://stackoverflow.com/a/7513356/938089
+ * @version      20131010
+ * @description  Executes function on a framed YouTube video (see website link)
+ *               For a full list of possible functions, see:
+ *               https://developers.google.com/youtube/js_api_reference
+ * @param String frame_id The id of (the div containing) the frame
+ * @param String func     Desired function to call, eg. "playVideo"
+ *        (Function)      Function to call when the player is ready.
+ * @param Array  args     (optional) List of arguments to pass to function func*/
+function callPlayer(frame_id, func, args) {
+    if (window.jQuery && frame_id instanceof jQuery) frame_id = frame_id.get(0).id;
+    var iframe = document.getElementById(frame_id);
+    if (iframe && iframe.tagName.toUpperCase() != 'IFRAME') {
+        iframe = iframe.getElementsByTagName('iframe')[0];
+    }
+
+    // When the player is not ready yet, add the event to a queue
+    // Each frame_id is associated with an own queue.
+    // Each queue has three possible states:
+    //  undefined = uninitialised / array = queue / 0 = ready
+    if (!callPlayer.queue) callPlayer.queue = {};
+    var queue = callPlayer.queue[frame_id],
+        domReady = document.readyState == 'complete';
+
+    if (domReady && !iframe) {
+        // DOM is ready and iframe does not exist. Log a message
+        window.console && console.log('callPlayer: Frame not found; id=' + frame_id);
+        if (queue) clearInterval(queue.poller);
+    } else if (func === 'listening') {
+        // Sending the "listener" message to the frame, to request status updates
+        if (iframe && iframe.contentWindow) {
+            func = '{"event":"listening","id":' + JSON.stringify(''+frame_id) + '}';
+            iframe.contentWindow.postMessage(func, '*');
+        }
+    } else if (!domReady ||
+               iframe && (!iframe.contentWindow || queue && !queue.ready) ||
+               (!queue || !queue.ready) && typeof func === 'function') {
+        if (!queue) queue = callPlayer.queue[frame_id] = [];
+        queue.push([func, args]);
+        if (!('poller' in queue)) {
+            // keep polling until the document and frame is ready
+            queue.poller = setInterval(function() {
+                callPlayer(frame_id, 'listening');
+            }, 250);
+            // Add a global "message" event listener, to catch status updates:
+            messageEvent(1, function runOnceReady(e) {
+                if (!iframe) {
+                    iframe = document.getElementById(frame_id);
+                    if (!iframe) return;
+                    if (iframe.tagName.toUpperCase() != 'IFRAME') {
+                        iframe = iframe.getElementsByTagName('iframe')[0];
+                        if (!iframe) return;
+                    }
+                }
+                if (e.source === iframe.contentWindow) {
+                    // Assume that the player is ready if we receive a
+                    // message from the iframe
+                    clearInterval(queue.poller);
+                    queue.ready = true;
+                    messageEvent(0, runOnceReady);
+                    // .. and release the queue:
+                    while (tmp = queue.shift()) {
+                        callPlayer(frame_id, tmp[0], tmp[1]);
+                    }
+                }
+            }, false);
+        }
+    } else if (iframe && iframe.contentWindow) {
+        // When a function is supplied, just call it (like "onYouTubePlayerReady")
+        if (func.call) return func();
+        // Frame exists, send message
+        iframe.contentWindow.postMessage(JSON.stringify({
+            "event": "command",
+            "func": func,
+            "args": args || [],
+            "id": frame_id
+        }), "*");
+    }
+    /* IE8 does not support addEventListener... */
+    function messageEvent(add, listener) {
+        var w3 = add ? window.addEventListener : window.removeEventListener;
+        w3 ?
+            w3('message', listener, !1)
+        :
+            (add ? window.attachEvent : window.detachEvent)('onmessage', listener);
+    }
+}
+
